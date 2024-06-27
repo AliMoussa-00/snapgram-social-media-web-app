b068f230f814ee37e1e9b53ecf162b043a847f64
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-06-27 11:52:33 +0100

    On branch ali_feature_user_integration Your branch is up to date with 'origin/ali_feature_user_integration'.
    Changes to be committed:
    	modified:   app/models/token.py
    		- add comments

--- backend/app/models/token.py
+++ backend/app/models/token.py
@@ -8,12 +8,27 @@ from pydantic import BaseModel, Field
 
 
 class Token(BaseModel):
+    """
+    Represents a token pair for authentication.
+
+    Attributes:
+        access_token (str): The access token used for authenticating requests.
+        refresh_token (str): The refresh token used for obtaining a new access token.
+        token_type (Optional[str]): The type of token, default is 'bearer'.
+    """
     access_token: str
     refresh_token: str
     token_type: Optional[str] = 'bearer'
 
 
 class BlackListedTokens(Document):
+    """
+    Represents a blacklisted token, which will be used in logout.
+
+    Attributes:
+        token (str): The blacklisted token.
+        black_listed_on (datetime): The date and time when the token was blacklisted.
+    """
     token: str
     black_listed_on: datetime = Field(default_factory=datetime.now)
 
@@ -22,6 +37,15 @@ class BlackListedTokens(Document):
 
     @classmethod
     async def is_token_blacklisted(cls, token: str) -> bool:
-        black_listed_token = await cls.find_one({"token": token})
+        """
+        Checks if a token is blacklisted.
 
+        Args:
+            token (str): The token to check.
+
+        Returns:
+            bool: True if the token is blacklisted, False otherwise.
+        """
+
+        black_listed_token = await cls.find_one({"token": token})
         return black_listed_token is not None
